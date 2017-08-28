07ea7cc15d8821fc1f31a88e9ebd3934fb76afc5
jbk77 <josephbk.117@hotmail.com>
2017-08-28 19:51:43 +0530

    Adding error checking and support for shaders

--- 2D Game Engine/ShaderProgram.cpp
+++ 2D Game Engine/ShaderProgram.cpp
@@ -4,9 +4,46 @@
 
 ShaderProgram::ShaderProgram()
 {
+	programID = 0;
+	vertexShaderID = 0;
+	fragmentShaderID = 0;
 }
 
 
 ShaderProgram::~ShaderProgram()
+{
+}
+
+void ShaderProgram::compileShader(const std::string & vertexShaderPath, const std::string & fragmentShaderPath)
+{
+	vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	if (vertexShaderID == 0)
+		std::cout << "ERROR : Vertex shader creation";
+	fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	if (vertexShaderID == 0)
+		std::cout << "ERROR : Fragment shader creation";
+
+	std::ifstream vertexFile(vertexShaderPath);
+	if (vertexFile.fail())
+	{
+		perror(vertexShaderPath.c_str());
+		std::cout << "ERROR : Vertex file couldnt be loaded";
+	}
+	std::string fileContents = "";
+	std::string line;
+	while (std::getline(vertexFile, line))
+	{
+		fileContents += line + "\n";
+	}
+	vertexFile.close();
+	const char* charPointer = fileContents.c_str();
+	glShaderSource(vertexShaderID, 1, &charPointer, nullptr);
+	glCompileShader(vertexShaderID);
+
+	GLint success = 0;
+	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &success);
+}
+
+void ShaderProgram::linkShaders()
 {
 }
--- 2D Game Engine/ShaderProgram.h
+++ 2D Game Engine/ShaderProgram.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+#include <GL\glew.h>
+#include <fstream>
 #include <string>
 class ShaderProgram
 {
@@ -7,5 +10,7 @@ public:
 	~ShaderProgram();
 	void compileShader(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
 	void linkShaders();
+private:
+	GLuint programID, vertexShaderID, fragmentShaderID;
 };
 
