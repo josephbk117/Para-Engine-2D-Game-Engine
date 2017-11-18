00bd810cbeac5f2d81ad863ef90a7db6e4bea973
jbk77 <josephbk.117@hotmail.com>
2017-11-18 13:13:29 +0530

    font rendering capability almost reached

--- 2D Game Engine/Game.cpp
+++ 2D Game Engine/Game.cpp
@@ -38,9 +38,9 @@ unsigned char temp_bitmap[512 * 512];
 
 stbtt_bakedchar cdata[96]; // ASCII 32..126 is 95 glyphs
 GLuint ftex;
-
 void my_stbtt_initfont(void)
 {
+
 	fread(ttf_buffer, 1, 1 << 20, fopen("Test Resources\\arial.ttf", "rb"));
 	stbtt_BakeFontBitmap(ttf_buffer, 0, 32.0, temp_bitmap, 512, 512, 32, 96, cdata); // no guarantee this fits!
 																					 // can free ttf_buffer at this point
@@ -57,14 +57,32 @@ void my_stbtt_print(float x, float y, char *text)
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, ftex);
 	glBegin(GL_QUADS);
+
 	while (*text) {
 		if (*text >= 32 && *text < 128) {
 			stbtt_aligned_quad q;
 			stbtt_GetBakedQuad(cdata, 512, 512, *text - 32, &x, &y, &q, 1);//1=opengl & d3d10+,0=d3d9
-			glTexCoord2f(q.s0, q.t1); glVertex2f(q.x0/500, q.y0/500);
-			glTexCoord2f(q.s1, q.t1); glVertex2f(q.x1/500, q.y0/500);
-			glTexCoord2f(q.s1, q.t0); glVertex2f(q.x1/500, q.y1/500);
-			glTexCoord2f(q.s0, q.t0); glVertex2f(q.x0/500, q.y1/500);
+			if (*text == 'g' || *text == 'j' || *text == 'p' || *text == 'q' || *text == 'y')
+			{
+				glTexCoord2f(q.s0, q.t1); glVertex2f(q.x0 / 500, (q.y0 - 6.0f) / 500);
+				glTexCoord2f(q.s1, q.t1); glVertex2f(q.x1 / 500, (q.y0 - 6.0f) / 500);
+				glTexCoord2f(q.s1, q.t0); glVertex2f(q.x1 / 500, (q.y1 - 6.0f) / 500);
+				glTexCoord2f(q.s0, q.t0); glVertex2f(q.x0 / 500, (q.y1 - 6.0f) / 500);
+			}
+			else if (cdata[(int)*text - 32].yoff < -16)
+			{
+				glTexCoord2f(q.s0, q.t1); glVertex2f(q.x0 / 500, (q.y0 + 4.0f) / 500);
+				glTexCoord2f(q.s1, q.t1); glVertex2f(q.x1 / 500, (q.y0 + 4.0f) / 500);
+				glTexCoord2f(q.s1, q.t0); glVertex2f(q.x1 / 500, (q.y1 + 4.0f) / 500);
+				glTexCoord2f(q.s0, q.t0); glVertex2f(q.x0 / 500, (q.y1 + 4.0f) / 500);
+			}
+			else
+			{
+				glTexCoord2f(q.s0, q.t1); glVertex2f(q.x0 / 500, q.y0 / 500);
+				glTexCoord2f(q.s1, q.t1); glVertex2f(q.x1 / 500, q.y0 / 500);
+				glTexCoord2f(q.s1, q.t0); glVertex2f(q.x1 / 500, q.y1 / 500);
+				glTexCoord2f(q.s0, q.t0); glVertex2f(q.x0 / 500, q.y1 / 500);
+			}
 		}
 		++text;
 	}
