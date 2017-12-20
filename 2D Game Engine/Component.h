28bcf7822defe98c922b17bdfef5a919e6677594
jbk77 <josephbk.117@hotmail.com>
2017-12-20 23:27:32 +0530

    minor tweak

--- 2D Game Engine/Component.h
+++ 2D Game Engine/Component.h
@@ -6,7 +6,7 @@ class Component
 public:
 	virtual void start() { };
 	virtual void update() { };
-	virtual void collisionStarted(GameObject* gameObject) noexcept { };
-	virtual void collisionEnded(GameObject* gameObject) noexcept { };
+	virtual void collisionStarted(GameObject* gameObject) { };
+	virtual void collisionEnded(GameObject* gameObject) { };
 	GameObject* attachedGameObject = nullptr;
 };
