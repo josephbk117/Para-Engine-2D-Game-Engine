1fdcd76e8ac89efb69de6ffea42135eca89bc633
jbk77 <josephbk.117@hotmail.com>
2018-01-23 00:58:11 +0530

    image now shown in correct orientation and correct resolution values

--- Para Engine Editor/PropertyPanel.cpp
+++ Para Engine Editor/PropertyPanel.cpp
@@ -26,27 +26,28 @@ void PropertyPanel::display(int screenWidth, int screenHeight)
 	ImTextureID my_tex_id = (void *)textures[0].textureId; //io.Fonts->TexID;
 	float my_tex_w, my_tex_h;
 	float ratio;
+	const float MAX_SIZE = 145.0f;
 	if (textures[0].width > textures[0].height)
-		ratio = 120.0f / textures[0].width;
+		ratio = MAX_SIZE / textures[0].width;
 	else
-		float ratio = 120.0f / textures[0].height;
+		float ratio = MAX_SIZE / textures[0].height;
 	my_tex_w = ratio * textures[0].width;
 	my_tex_h = ratio * textures[0].height;
 
-	ImGui::Text("%.0fx%.0f", my_tex_w, my_tex_h);
+	ImGui::Text("%dx%d", textures[0].width, textures[0].height);
 	ImVec2 pos = ImGui::GetCursorScreenPos();
-	ImGui::Image(my_tex_id, ImVec2(my_tex_w, my_tex_h), ImVec2(0, 0), ImVec2(1, 1), ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 128));
+	ImGui::Image(my_tex_id, ImVec2(my_tex_w, my_tex_h), ImVec2(1, 1), ImVec2(0, 0), ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 128));
 	if (ImGui::IsItemHovered())
 	{
 		ImGui::BeginTooltip();
 		float focus_sz = 32.0f;
 		float focus_x = io.MousePos.x - pos.x - focus_sz * 0.5f; if (focus_x < 0.0f) focus_x = 0.0f; else if (focus_x > my_tex_w - focus_sz) focus_x = my_tex_w - focus_sz;
 		float focus_y = io.MousePos.y - pos.y - focus_sz * 0.5f; if (focus_y < 0.0f) focus_y = 0.0f; else if (focus_y > my_tex_h - focus_sz) focus_y = my_tex_h - focus_sz;
-		ImGui::Text("Min: (%.2f, %.2f)", focus_x, focus_y);
-		ImGui::Text("Max: (%.2f, %.2f)", focus_x + focus_sz, focus_y + focus_sz);
+		ImGui::Text("Min: (%.2f, %.2f)", focus_x/ratio, focus_y/ratio);
+		ImGui::Text("Max: (%.2f, %.2f)", (focus_x + focus_sz)/ratio, (focus_y + focus_sz)/ratio);
 		ImVec2 uv0 = ImVec2((focus_x) / my_tex_w, (focus_y) / my_tex_h);
 		ImVec2 uv1 = ImVec2((focus_x + focus_sz) / my_tex_w, (focus_y + focus_sz) / my_tex_h);
-		ImGui::Image(my_tex_id, ImVec2(128, 128), uv0, uv1, ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 128));
+		ImGui::Image(my_tex_id, ImVec2(128, 128), uv1, uv0, ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 128));
 		ImGui::EndTooltip();
 	}
 
